ebb15d5544a8049a5629b1decce59cadaba8c625
Iuri Sampaio <iuri@iurix.com>
2020-07-05 01:00:05 -0300

    edited API method /get

--- packages/ctree-core/www/get.tcl
+++ packages/ctree-core/www/get.tcl
@@ -1,4 +1,10 @@
-ad_page_contract {} {    
+#/packages/ctree-core/www/get.tcl
+ad_page_contract {
+    Get data based on parameters set for URL.
+
+    @author Iuri de Araujo (iuri@iurix.com)
+    @creation_date 4 Jul 2020
+} {    
     {cTree:optional}
     {cTreeName:boolean,optional}
     {description:optional}
@@ -13,11 +19,12 @@ ad_page_contract {} {
     {pageOffset ""}	
 }
 
-if {[ix_rest::jwt::validation_p] eq 0} {
+if {[ctree::jwt::validation_p] eq 0} {
     ad_return_complaint 1 "Bad HTTP Request: Invalid Token!"
     ns_respond -status 400 -type "text/html" -string "Bad Request Error HTML 400. The server cannot or will not process the request due to an apparent client error (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing."
     ad_script_abort
 }
+
 
 
 if {[ns_conn method] eq "GET"} {
@@ -59,11 +66,16 @@ if {[ns_conn method] eq "GET"} {
 		append json_data "\"cTreeName\": \"$item(title)\""
 	    }
 	    
-	    # If a ctree_post is required, then return post's data 
+	    # If a ctree_post is required, then return post's data
+	    ### listDataKeys
+	    #### BEGIN
+
+	    ##
+	    ##### postType
+	    ##
 	    if {[info exists postType]} {
-		ns_log Notice "postType TRUE"
 		append json_request "\"postType\": \"id\","
-		append json_data "\",postType\": \["
+		append json_data ",\"postType\": \["
 
 		# Gets all postTypes
 		db_foreach select_posts {
@@ -82,7 +94,7 @@ if {[ns_conn method] eq "GET"} {
 			\"parentsMax\": 0,
 			\"iconUrl\": \"/images/post_type_icon.png\",
 			\"color\": \"#FF7700\", 
-			\"description\": \"$description\",
+			\"description\": \"Sample of description\",
 			\"prompt\": \"Button label to add post with type\"
 		    \},"		    
 		}
@@ -91,12 +103,11 @@ if {[ns_conn method] eq "GET"} {
 		append json_data "\]"
 	    }	    
 
-
+	    ##
+	    ##### segmentType
+	    ##
 	    if {[info exists segmentType]} {
-		ns_log notice "segmentType TRUE"
 		append json_request "\"segmentType\": \"id\","
-
-
 		append json_data ",\"segmentType\": \["
 
 		# Gets all postTypes
@@ -117,12 +128,56 @@ if {[ns_conn method] eq "GET"} {
 		}
 		
 		set json_data [string trimright $json_data ","]
-		append json_data "\]"
+		append json_data "\]"		
+	    }
 
 
+	    ##
+	    ##### segmentType
+	    ##
+	    if {[info exists segmentType]} {
+		append json_request "\"segmentType\": \"id\","
+		append json_data ",\"segmentType\": \["
 
+		# Gets all postTypes
+		db_foreach select_segment_type {
+		    SELECT ci.item_id AS id, ci.name, cr.title, cr.description
+		    FROM cr_items ci, cr_revisions cr
+		    WHERE ci.item_id = cr.item_id
+		    AND ci.latest_revision = cr.revision_id
+		    AND ci.content_type = 'ctree_segmenttype'
+		    AND ci.parent_id = :item_id
+		} {
+		    ns_log Notice "SEgmentType $id"
+		    append json_data "\{
+			\"id\": \"$name\",
+			\"componentName\": \"example-type-component\",
+			\"canBeThumbnail\": true
+		    \},"		    
+		}
 		
-	    }	    
+		set json_data [string trimright $json_data ","]
+		append json_data "\]"	       
+	    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	    
 
 	    set json_request [string trimright $json_request ","]
 	    set json_data [string trimright $json_data ","]
@@ -181,30 +236,30 @@ if {[ns_conn method] eq "GET"} {
 	
     
 } else {
-
-	set result "{
-                  \"data\": {
-                               \"status\":false
-                  },
-                  \"errors\":{
-                               \"id\": \"401 Unauthorized\",
-                               \"status\": \"HTTP/1.1 401 Access Denied\",
-                               \"title\": \"Invalid Token.\",
-                               \"detail\": \"The token sent does not relate to any data in the system. Please correct token and try again! \",
-                               \"source\": \"filename: /packages/ctree/www/get.tcl \"
-                  },
-                  \"meta\": {
-                         \"copyright\": \"Copyright 2019 Collaboration Tree http://www.innovativefuture.org/collaboration-tree/ \",
-                         \"application\": \"CTree Rest API\",
-                               \"version\": \"0.1d\",
-                               \"id\": \"HTTP/1.1 401 Unauthorized\",
-                               \"status\": \"false\",
-                               \"message\": \"Invalid Token. Access denied.\"
-                  }                                                                                                 
-        }"
-
-
-	doc_return 401 "application/json" $result
-	ad_return_complaint 1 "Item URL Invalid"
-	ad_script_abort
+    
+    set result "\{
+	\"data\": \{
+	    \"status\":false
+	\},
+	\"errors\": \{
+	    \"id\": \"401 Unauthorized\",
+	    \"status\": \"HTTP/1.1 401 Access Denied\",
+	    \"title\": \"Invalid Token.\",
+	    \"detail\": \"The token sent does not relate to any data in the system. Please correct token and try again! \",
+	    \"source\": \"filename: /packages/ctree/www/get.tcl \"
+	\},
+	\"meta\": \{
+	    \"copyright\": \"Copyright 2019 Collaboration Tree http://www.innovativefuture.org/collaboration-tree/ \",
+	    \"application\": \"CTree Rest API\",
+	    \"version\": \"0.1d\",
+	    \"id\": \"HTTP/1.1 401 Unauthorized\",
+	    \"status\": \"false\",
+	    \"message\": \"Invalid Token. Access denied.\"
+	\}      
+    \}"
+    
+    
+    doc_return 401 "application/json" $result
+    ad_return_complaint 1 "Item URL Invalid"
+    ad_script_abort
 }

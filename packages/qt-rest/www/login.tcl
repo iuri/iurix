3beb25e9e6bf8a209545c46109a62c90134fb496
Iuri de Araujo <iuri.dearaujo@qonteo.com>
2020-08-06 22:47:14 -0300

    removed logs from login REST method

--- packages/qt-rest/www/login.tcl
+++ packages/qt-rest/www/login.tcl
@@ -3,22 +3,13 @@ ns_log Notice "Running TCL script user/login"
 if {[ns_conn method] eq "POST"} {
     package req json
 
-    set header [ns_conn header]
-    ns_log Notice "HEADER \n $header"
-    set h [ns_set size $header]
-    ns_log Notice "HEADERS $h"
-    set req [ns_set array $header]
-    ns_log Notice "$req"
-    
     set dict [json::json2dict [ns_getcontent -as_file false]]
     #
     # Do something with the dict
     #
-    ns_log Notice "DICT $dict"
 
     array set arr $dict
     if {[array exists arr] && [array size arr] > 0} {
-	ns_log Notice "EMAIL $arr(email) \n PWD  $arr(password) \n"
 
 	set authority_options [auth::authority::get_authority_options]
 	set authority_id [lindex $authority_options 0 1]
@@ -36,25 +27,18 @@ if {[ns_conn method] eq "POST"} {
 		
 		acs_user::get -user_id $auth_info(user_id) -array user
 
-		ns_log Notice "[parray user] "
-
 		## Begin ad_proc jwt
 		
 		set header "{\"alg\": \"HS256\", \"typ\": \"JWT\"}"
-		set enc_header [ns_base64encode $header]
-		ns_log Notice "HEADER $header"
-		
+		set enc_header [ns_base64encode $header]		
 		# set payload "\{\"iss\": \"26973410000102\", \"aud\": \"iurix.com/REST\", \"sub\": \"$user(user_id)\", \"name\": \"$user(name)\", \"iat\": [ns_time], \"exp\": [ns_time incr [ns_time] 6000]\}"
 		# set payload "{'iss': '26973410000102', 'aud': 'iurix.com/REST', 'sub': '$user(user_id)', 'iat': [ns_time]}"
 		set payload "{'sub': '$user(user_id)', 'iat': [ns_time]}"
-		ns_log Notice "Payload $payload"
 		set enc_payload [ns_base64encode $payload]
 
 		set hmac_secret [ns_crypto::hmac string -digest sha256 "Abracadabra" "What is the magic word?"]
-		ns_log Notice "CRYPTO $hmac_secret"
 
 		set token "${enc_header}.${enc_payload}.${hmac_secret}"
-		ns_log Notice "TOIKEN $token"	
 
 		## Finish ADproc from ix-jwt
 		

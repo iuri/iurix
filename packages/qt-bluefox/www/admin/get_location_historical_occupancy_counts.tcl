d1891839abdc2ef611fda31baeeb9dcc0aaaa606
Iuri de Araujo <iuri.dearaujo@qonteo.com>
2021-02-11 16:48:50 -0300

    edited feature to import data from bluefox

--- packages/qt-bluefox/www/admin/get_location_historical_occupancy_counts.tcl
+++ packages/qt-bluefox/www/admin/get_location_historical_occupancy_counts.tcl
@@ -38,18 +38,69 @@ array set arr $l
 #status OK occupancy_count_flooring_enabled 1 floor_occupancy_count 0 occupancy_count_slots
 
 #ns_log Notice "$occupancy_count_slots"
-
+set total 0
+set l_totals [list] 
 foreach elem $arr(occupancy_count_slots) {
-    ns_log Notice "ELEM $elem"
-    set timestamp [lindex $elem 0]
+    
+    set timestamp [lindex $elem 0]	
     set date_from [db_string convert_timestamp {
 	SELECT TIMESTAMP WITH TIME ZONE 'epoch' + :timestamp * INTERVAL '1 second'
     }]
-    set timestamp [lindex $elem 1]
-    set date_to [db_string convert_timestamp {
-	SELECT TIMESTAMP WITH TIME ZONE 'epoch' + :timestamp * INTERVAL '1 second'
-    }]
     
-    ns_log Notice "DATEFROM $date_from | DATETO $date_to"
+    
+    
+    set day_hour [lindex [split $date_from ":"] 0]
+    set idx [lsearch -index 0 $l_totals "${day_hour}:00:00"] 
+    set total [expr $total + [expr [expr [lindex $elem 2] + [lindex $elem 3]] / 2.00]]
+
+
+    
+    if { $idx eq -1 } {
+	if {[lindex $elem 2] eq 0 && [lindex $elem 3] eq 0} {
+	    # do nothing
+	} else {
+	    ns_log Notice "INSERT PARTIAL"
+	    set count [expr [expr [lindex $elem 2] + [lindex $elem 3]] / 2.00]
+	    lappend l_totals [list "${day_hour}:00:00" $count]
+
+	    ns_log Notice "$day_hour | $idx | [lindex $elem 2] | [lindex $elem 3] | TOTAL $total"
+
+	}
+    } else {
+	ns_log Notice "SUM PARTIAL"
+	set old_count [lindex [lindex $l_totals $idx] 1]
+	set count [expr [expr [lindex $elem 2] + [lindex $elem 3]] / 2.00]
+	lset l_totals $idx [list "${day_hour}:00:00" [expr $count + $old_count]]
+
+	ns_log Notice "$day_hour | $idx | [lindex $elem 2] | [lindex $elem 3] | TOTAL $total"
+
+    }		  
+	
+    
+}
+
+ns_log Notice "RESULT ****"
+foreach elem $l_totals {
+#    ns_log Notice "ELEM $elem"
+    set hour [lindex $elem 0]
+    set total [expr round([lindex $elem 1])]
+#    set total [expr [lindex $elem 1]]
+
+    ns_log Notice "INSERT $hour TOTAL $total "
+
+
+    
+    db_transaction {
+	db_exec_plsql insert_totals {
+	    SELECT qt_totals__new(:hour,
+				  :total,
+				  null,
+				  null,
+				  null,
+				  'CCPN003',
+				  'qt_face')		    
+	}
+    }
+    
 }
 

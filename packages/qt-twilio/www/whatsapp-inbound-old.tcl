a48c85428c680375e3cbb88fa0f8ec27a93b4554
Iuri de Araujo <iuri.dearaujo@qonteo.com>
2021-03-06 23:05:46 -0300

    edited whatsapp twilio bot

--- packages/qt-twilio/www/whatsapp-inbound-old.tcl
+++ packages/qt-twilio/www/whatsapp-inbound-old.tcl
@@ -31,12 +31,42 @@ ad_page_contract {
     {ApiVersion ""}
 }
 
+ns_log notice "Running TCL script /packages/qt-twilio/www/whatsapp-inbound.tcl"
+
+ns_log Notice "RECEIVE POST - Request Message from Twilio API"
+
+
+set myform [ns_getform]
+if {[string equal "" $myform]} {
+    ns_log Notice "No Form was submited"
+} else {
+    ns_log Notice "FORM"
+    ns_set print $myform
+    for {set i 0} {$i < [ns_set size $myform]} {incr i} {
+	set varname [ns_set key $myform $i]
+	set varvalue [ns_set value $myform $i]
+	ns_log Notice " $varname - $varvalue"
+    }
+}
+
+
+
+
+
+set header [ns_conn header]
+#ns_log Notice "HEADER \n $header"
+set h [ns_set size $header]
+#ns_log Notice "HEADERS $h"
+set req [ns_set array $header]
+ns_log Notice "$req"
+
+#set content  [ns_getcontent -as_file false]
+#ns_log Notice "COENTNT $content"
 
 set phonenumber [lindex [split $From ":"] 1]
 db_0or1row select_user_id {
     SELECT user_id AS creation_user
-    FROM user_ext_info
-    WHERE phonenumber = :phonenumber
+    FROM user_ext_info WHERE phonenumber = :phonenumber
 }
 
 
@@ -45,49 +75,36 @@ db_0or1row select_messages {
     FROM qt_whatsapp_msg_tx
     WHERE creation_user = :creation_user
 }
+ns_log Notice "COUNT $count *****"
+
+
 
 set package_id [apm_package_id_from_key qt-twilio]
-
-set username [parameter::get_global_value -package_key qt-twilio -parameter AccountSID -default ""]
-set token [parameter::get_global_value -package_key qt-twilio -parameter AuthToken -default ""]
-set source [parameter::get_global_value -package_key qt-twilio -parameter WhatsAppDefaultNumber -default ""]
-
-
-set url "https://api.twilio.com/2010-04-01/Accounts/${username}/Messages.json"
-#set url "https://dashboard.qonteo.com/twilio/whatsapp"
-
-set auth_token [join [ns_base64encode ${username}:${token}] ""]
-
-set req_headers [ns_set create]
-ns_set update $req_headers Authorization "Basic $auth_token"
-ns_set update $req_headers Content-Type "multipart/form-data"
-
-
-
 if {![db_0or1row item_exists {
     SELECT item_id FROM cr_items
     WHERE name = :MessageSid
     AND parent_id = :package_id
     AND content_type = 'qt_whatsapp_msg'
 }]} {
-    set description [list \
-			 [list SmsMessageSid $SmsMessageSid] \
-			 [list NumMedia $NumMedia] \
-			 [list ProfileName $ProfileName] \
-			 [list SmsSid $SmsSid] \
-			 [list WaId $WaId] \
-			 [list SmsStatus $SmsStatus] \
-			 [list Body $Body] \
-			 [list To $To] \
-			 [list NumSegments $NumSegments] \
-			 [list MessageSid $MessageSid] \
-			 [list AccountSid $AccountSid] \
-			 [list From $From] \
-			 [list ApiVersion $ApiVersion]]
     
     if {$count eq 1} {
 	if {[regexp {^([0-9]+)$} $Body] || [regexp {^([0-9]+)\,([0-9]+)$} $Body] } {
-	    db_transaction {	
+	    db_transaction {
+		set description [list \
+				     [list SmsMessageSid $SmsMessageSid] \
+				     [list NumMedia $NumMedia] \
+				     [list ProfileName $ProfileName] \
+				     [list SmsSid $SmsSid] \
+				     [list WaId $WaId] \
+				     [list SmsStatus $SmsStatus] \
+				     [list Body $Body] \
+				     [list To $To] \
+				     [list NumSegments $NumSegments] \
+				     [list MessageSid $MessageSid] \
+				     [list AccountSid $AccountSid] \
+				     [list From $From] \
+				     [list ApiVersion $ApiVersion]]
+		
 		
 		set item_id [content::item::new \
 				 -parent_id $package_id \
@@ -104,19 +121,44 @@ if {![db_0or1row item_exists {
 				 -mime_type "text/plain"
 			    ]
 	    }	    	    
-	} else {        	    
+	} else {
+    
+	    set username [parameter::get_global_value -package_key qt-twilio -parameter AccountSID -default ""]
+	    set token [parameter::get_global_value -package_key qt-twilio -parameter AuthToken -default ""]
+	    set source [parameter::get_global_value -package_key qt-twilio -parameter WhatsAppDefaultNumber -default ""]
+	    
+	    
+	    set url "https://api.twilio.com/2010-04-01/Accounts/${username}/Messages.json"
+	    #set url "https://dashboard.qonteo.com/twilio/whatsapp"
+	    
+	    set auth_token [join [ns_base64encode ${username}:${token}] ""]
+	    
+	    set req_headers [ns_set create]
+	    ns_set update $req_headers Authorization "Basic $auth_token"
+	    ns_set update $req_headers Content-Type "multipart/form-data"
+	    	    
 	   	 
 	    set body "Por favor, Envie solamente numeros!"
 	    set formvars [export_vars -url {{To "whatsapp:+5511998896571"} {From $source} {Body $body}}]
 	    set res [util::http::post -url $url -headers $req_headers -formvars $formvars -multipart]
-
-	    ns_respond -status 200 -type "text/html" -string ""
-	    ad_script_abort
-	    
 	}
     } else {
 
 	db_transaction {
+	    set description [list \
+				 [list SmsMessageSid $SmsMessageSid] \
+				 [list NumMedia $NumMedia] \
+				 [list ProfileName $ProfileName] \
+				 [list SmsSid $SmsSid] \
+				 [list WaId $WaId] \
+				 [list SmsStatus $SmsStatus] \
+				 [list Body $Body] \
+				 [list To $To] \
+				 [list NumSegments $NumSegments] \
+				 [list MessageSid $MessageSid] \
+				 [list AccountSid $AccountSid] \
+				 [list From $From] \
+				 [list ApiVersion $ApiVersion]]			       
 	    set item_id [content::item::new \
 			     -parent_id $package_id \
 			     -creation_user $creation_user \
@@ -152,7 +194,20 @@ db_0or1row select_messages {
     
 
 if {$count < 6} {	
-        
+    
+    set username [parameter::get_global_value -package_key qt-twilio -parameter AccountSID -default ""]
+    set token [parameter::get_global_value -package_key qt-twilio -parameter AuthToken -default ""]
+    set source [parameter::get_global_value -package_key qt-twilio -parameter WhatsAppDefaultNumber -default ""]
+    
+    
+    set url "https://api.twilio.com/2010-04-01/Accounts/${username}/Messages.json"
+    
+    set auth_token [join [ns_base64encode ${username}:${token}] ""]
+    
+    set req_headers [ns_set create]
+    ns_set update $req_headers Authorization "Basic $auth_token"
+    ns_set update $req_headers Content-Type "multipart/form-data"
+    
     if {[info exists creation_user]} {
 	db_0or1row select_user_names {
 	    SELECT first_names, last_name FROM cc_users WHERE user_id = :creation_user
--- packages/qt-twilio/www/whatsapp-status.tcl
+++ packages/qt-twilio/www/whatsapp-status.tcl
@@ -14,17 +14,17 @@ ad_page_contract {} {
     {ChannelInstallSid ""}
 }
 
-ns_log Notice "Running TCL script # /packages/qt-twilio/www/whatsapp-status.tcl"
+#ns_log Notice "Running TCL script # /packages/qt-twilio/www/whatsapp-status.tcl"
 
 set header [ns_conn header]
 #ns_log Notice "HEADER \n $header"
 set h [ns_set size $header]
 #ns_log Notice "HEADERS $h"
 set req [ns_set array $header]
-ns_log Notice "$req"
+#ns_log Notice "$req"
 
 set content  [ns_getcontent -as_file false]
-ns_log Notice "COENTNT $content"
+#ns_log Notice "COENTNT $content"
 
 
 
@@ -33,12 +33,12 @@ set myform [ns_getform]
 if {[string equal "" $myform]} {
     ns_log Notice "No Form was submited"
 } else {
-    ns_log Notice "FORM"
-    ns_set print $myform
+    #   ns_log Notice "FORM"
+    #    ns_set print $myform
     for {set i 0} {$i < [ns_set size $myform]} {incr i} {
 	set varname [ns_set key $myform $i]
 	set varvalue [ns_set value $myform $i]
-	ns_log Notice " $varname - $varvalue"
+#	ns_log Notice " $varname - $varvalue"
     }
 }
 
